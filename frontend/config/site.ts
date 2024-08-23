7b87af43f9257430e24b45a63dbad56e50cf19c3
Adithya S K <adithyaskolavi@gmail.com>
2024-08-24 02:36:44 +0530

    utility folders init - Adithya S K

---
+++ frontend/config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from '@/types';
+
+export const siteConfig: SiteConfig = {
+  name: 'RAG SAAS',
+  description: 'RAG-SaaS: Ship RAG Solutions Quickly',
+  url: 'https://rag.cognitivelab.in/',
+  ogImage: 'https://rag.cognitivelab.in/og.jpg',
+  links: {
+    twitter: 'https://x.com/cognitivelab_ai',
+    github: 'https://github.com/adithya-s-k/RAG-SaaS',
+  },
+};
