e38b68d8798bd04c7cb60cac63526dcc9b2fb8e2
Adithya S K <adithyaskolavi@gmail.com>
2024-08-26 03:34:26 +0530

    configuration updated - Adithya S K

--- backend/app/api/chat/chat_config.py
+++ backend/app/api/chat/chat_config.py
@@ -15,34 +15,7 @@ logger = logging.getLogger("uvicorn")
 async def chat_config() -> ChatConfig:
     starter_questions = None
     conversation_starters = os.getenv("CONVERSATION_STARTERS")
+    # conversation_starters = "Tell me about CognitiveLab \n What are some open source projects  \n Tell me about Open Source AI \n What are AI Agents"
     if conversation_starters and conversation_starters.strip():
         starter_questions = conversation_starters.strip().split("\n")
     return ChatConfig(starter_questions=starter_questions)
-
-
-try:
-    from app.engine.service import LLamaCloudFileService
-
-    logger.info("LlamaCloud is configured. Adding /config/llamacloud route.")
-
-    @r.get("/llamacloud")
-    async def chat_llama_cloud_config():
-        projects = LLamaCloudFileService.get_all_projects_with_pipelines()
-        pipeline = os.getenv("LLAMA_CLOUD_INDEX_NAME")
-        project = os.getenv("LLAMA_CLOUD_PROJECT_NAME")
-        pipeline_config = None
-        if pipeline and project:
-            pipeline_config = {
-                "pipeline": pipeline,
-                "project": project,
-            }
-        return {
-            "projects": projects,
-            "pipeline": pipeline_config,
-        }
-
-except ImportError:
-    logger.debug(
-        "LlamaCloud is not configured. Skipping adding /config/llamacloud route."
-    )
-    pass
