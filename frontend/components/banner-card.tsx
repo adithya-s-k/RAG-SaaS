7f574b0cb57b63bc1ed6fb751701e7dfb63399e8
Adithya S K <adithyaskolavi@gmail.com>
2024-08-26 03:32:20 +0530

    modified banner - Adithya S K

--- frontend/components/banner-card.tsx
+++ frontend/components/banner-card.tsx
@@ -6,34 +6,35 @@ import { Button, buttonVariants } from './ui/button';
 import { cn } from '@/lib/utils';
 import { siteConfig } from '@/config/site';
 import { useTheme } from 'next-themes';
+import ShinyButton from './magicui/shiny-button';
 
 const BannerCard: React.FC = () => {
   const { theme, setTheme } = useTheme();
   return (
-    <div className="h-fit w-1/2 p-10 shadow-md bg-background rounded-xl  flex flex-col justify-center items-center gap-6">
-      <Image
-        src={
-          theme === 'dark'
-            ? '/images/banner_dark.png'
-            : '/images/banner_light.png'
-        }
-        alt="Image"
-        width="1080"
-        height="1080"
-        className="object-contain "
-      />
-      <h3>Proudly Open Source</h3>
-      <Link href={siteConfig.links.github} target="_blank" className="w-full">
-        <Button
-          className={cn(
-            buttonVariants({ variant: 'outline', size: 'icon' }),
-            'w-full text-primary'
-          )}
+    <div className="relative h-fit md:1/2 lg:w-2/5 overflow-hidden rounded-xl drop-shadow-md">
+      <div className="relative z-10 p-4 bg-background/80 backdrop-blur-sm flex flex-col justify-center items-center gap-6">
+        <Image
+          src={
+            theme === 'dark'
+              ? '/images/banner_dark.png'
+              : '/images/banner_light.png'
+          }
+          alt="Image"
+          width="1080"
+          height="1080"
+          className="object-contain px-16"
+        />
+        <h3 className="text-lg font-semibold w-full text-center">
+          Ship RAG Solutions ⚡Quickly
+        </h3>
+        <Link
+          href={siteConfig.links.github}
+          target="_blank"
+          className="w-full flex items-center justify-center"
         >
-          <Github className="mr-2 h-5 w-5" />
-          Github
-        </Button>
-      </Link>
+          <ShinyButton text="⭐ STAR US ON GITHUB " className="" />
+        </Link>
+      </div>
     </div>
   );
 };
