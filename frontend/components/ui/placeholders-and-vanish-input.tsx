f5b1bd654752e8933851b961b25e62c461a56b9b
Adithya S K <adithyaskolavi@gmail.com>
2024-08-26 23:53:43 +0530

    made search bar bigger - Adithya S K

--- frontend/components/ui/placeholders-and-vanish-input.tsx
+++ frontend/components/ui/placeholders-and-vanish-input.tsx
@@ -181,7 +181,7 @@ export function PlaceholdersAndVanishInput({
   return (
     <form
       className={cn(
-        'relative w-full md:max-w-[60%] mx-auto bg-white dark:bg-muted h-12 rounded-full overflow-hidden shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),_0px_1px_0px_0px_rgba(25,28,33,0.02),_0px_0px_0px_1px_rgba(25,28,33,0.08)] transition duration-200',
+        'relative w-full md:max-w-[60%] mx-auto bg-muted dark:bg-muted h-14 rounded-full overflow-hidden shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),_0px_1px_0px_0px_rgba(25,28,33,0.02),_0px_0px_0px_1px_rgba(25,28,33,0.08)] transition duration-200',
         value && 'bg-gray-50'
       )}
       onSubmit={handleSubmit}
