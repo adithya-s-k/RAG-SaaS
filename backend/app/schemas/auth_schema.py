e0af3364232c9f5f9fa145c0bdff4b39778f8849
Adithya S K <adithyaskolavi@gmail.com>
2024-08-27 13:26:19 +0530

    auth schema updated - Adithya S K

--- backend/app/schemas/auth_schema.py
+++ backend/app/schemas/auth_schema.py
@@ -1,11 +1,27 @@
 from uuid import UUID
 from pydantic import BaseModel
+from typing_extensions import Optional
+
 
 class TokenSchema(BaseModel):
     access_token: str
     refresh_token: str
-    
-    
+
+
 class TokenPayload(BaseModel):
     sub: UUID = None
     exp: int = None
+
+
+class ErrorOut(BaseModel):
+    detail: str
+    status_code: int
+
+
+class AuthErrorOut(ErrorOut):
+    error_description: Optional[str] = None
+    error_code: Optional[str] = None
+
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
